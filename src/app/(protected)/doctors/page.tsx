00ba63001aefc5b80dca6ebc7621080a72befed8
Vitor Mariotto <vitormariotto03@gmail.com>
2026-09-09 11:21:32 -0300

    feat: ensure doctors are listed page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -11,6 +11,8 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import AddDoctorButton from "./_components/add-doctor-button";
+import { db } from "@/db";
+import DoctorCard from "./_components/doctor-card";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
@@ -22,6 +24,12 @@ const DoctorsPage = async () => {
   if (!session?.user.clinic) {
     redirect("/clinic-form");
   }
+
+  const doctors = await db.query.doctorsTable.findMany({
+    where: {
+      clinicId: session.user.clinic.id,
+    },
+  });
   return (
     <PageContainer>
       <PageHeader>
@@ -33,7 +41,13 @@ const DoctorsPage = async () => {
           <AddDoctorButton />
         </PageActions>
       </PageHeader>
-      <PageContent>Médicos</PageContent>
+      <PageContent>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
+      </PageContent>
     </PageContainer>
   );
 };
